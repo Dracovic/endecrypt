67beb30fe60eb4c08884e7af5cbc704f74d4b647
Dracovic <vicbar94@gmail.com>
2018-01-22 13:35:55 -0600

    getopt intr

--- main.cpp
+++ main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <unistd.h>
 
-int main(){
-	std::cout << "Hello World" << std::endl;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int endeflag = 0;
+	char* fileName;
+	char *value = NULL;
+	int index;
+	int c;
+
+	while ((c = getopt (argc, argv, "e::d::f::a:")) != -1)
+		switch (c) {
+			case 'e':
+				endeflag = -1;
+				//cout << "Encrypt" << endl;
+				break;
+			case 'd':
+				endeflag = 1;
+				//cout << "Decrypt" << endl;
+				break;
+			case 'f':
+				fileName = argv[optind];
+				cout << fileName << endl;
+				break;
+			case 'a':
+				//cout << "This algo -> " << argv[optind-1] << endl;
+				break;
+			default:
+			break;
+		}
+				
 }
