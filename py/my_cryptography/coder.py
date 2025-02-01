40e63539798a35080c163cd8bfffab156a3d1044
Dracovic <vicbar94@gmail.com>
2025-02-01 03:08:50 -0600

    began major restructuring
    (cherry picked from commit 28f9d6e4523ebcb83df4e0390853bd6cd50dba7e)

--- py/my_cryptography/coder.py
+++ py/my_cryptography/coder.py
@@ -3,6 +3,7 @@ import string
 class Coder:
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     """ A Coder class to abstract manage and not repeat attributes both the Encoder and Decoder classes
         will have in common. Also will manage files and command-line inputs.
     """
@@ -25,15 +26,38 @@ class Coder:
             self.msg = "abcdefghijklmnopqrst"
             self.algo = "scytale"
 =======
+=======
+    """ A Coder class to abstract manage and not repeat attributes both the Encoder and Decoder classes
+        will have in common. Also will manage files and command-line inputs.
+    """
+>>>>>>> 28f9d6e (began major restructuring)
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.std_alphabet = dict([*zip(list(string.ascii_letters + string.digits), range(62))])
+<<<<<<< HEAD
 >>>>>>> 852295f (Created Coder Parent Class to abstract the argument passing from cmdline into both obj instancing without repeating code. Also generating the alphabet to be used for either class)
 =======
 
     def __init__(self):
         self.std_alphabet = dict([*zip(list(string.ascii_letters + string.digits), range(62))])
 >>>>>>> 852295f (Created Coder Parent Class to abstract the argument passing from cmdline into both obj instancing without repeating code. Also generating the alphabet to be used for either class)
+=======
+        if kwargs:
+            if kwargs["message"]:
+                self.msg = kwargs["message"]
+            elif kwargs["input"][:-4] == ".txt":
+                self.input = kwargs["input"]
+            if hasattr(self, kwargs["algo"]):
+                self.algo = getattr(self, kwargs["algo"])
+                if callable(self.algo):
+                    if kwargs["algo"] == "scytale":
+                        self.enc_msg = self.algo(kwargs["radius"])
+                    else:
+                        self.enc_msg = self.algo()
+        else: #No arguments passed programatically
+            self.msg = "abcdefghijklmnopqrst"
+            self.algo = "scytale"
+>>>>>>> 28f9d6e (began major restructuring)
 
 co = Coder()
 print(co.std_alphabet)
